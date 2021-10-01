df32f62c0574c00b291045523771b45bf0eb165f
Fahad Hussain <fahd__hussain@hotmail.com>
2021-10-02 02:10:30 +0500

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from 'process';
 import { AppModule } from './app.module';
+
+const PORT = env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Database Report Generating Tool V1')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
